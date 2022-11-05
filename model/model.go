f9807c6da97a0525b91ac89d1f07dfc5126b6a71
Yeqing <cuiyeqing960904@gmail.com>
2022-11-05 16:28:00 -0400

    base model setup

---
+++ model/model.go
@@ -0,0 +1,9 @@
+package model
+
+type Goly struct {
+	ID       uint64 `json:"id" gorm:"primaryKey"`
+	Redirect string `json:"redirect" gorm:"not null"`
+	Goly     string `json:"goly" gorm:"unique;not null"`
+	Clicked  uint64 `json:"clicked"`
+	Random   bool   `json:"random"`
+}
